5c8ec77c90c2b949e76cda4c9a3f451f63078af8
Shaharier Hoque Sathil <77714180+Sathil-Shaharier@users.noreply.github.com>
2021-09-28 22:43:23 +0600

    Update rental_information-3.sql

--- rental_information-3.sql
+++ rental_information-3.sql
@@ -90,8 +90,8 @@ CREATE TABLE `car_normal` (
 --
 
 INSERT INTO `car_normal` (`Product_id`, `National_id`, `Phone_number`, `Model`, `Bike_condition`, `Rent_Cost`, `Availability`, `Seat`) VALUES
-(30001, 2345678, '01765968783', 'Toyota Corola', '2010 KM and good CONDITION', '2000 per day', 'Yes', 4),
-(30002, 2345678, '01765968783', 'Toyota NOAH', '2410 KM and good CONDITION', '3500 per day', 'Yes', 8);
+(30001, 2345678, '017********', 'Toyota Corola', '2010 KM and good CONDITION', '2000 per day', 'Yes', 4),
+(30002, 2345678, '017********', 'Toyota NOAH', '2410 KM and good CONDITION', '3500 per day', 'Yes', 8);
 
 -- --------------------------------------------------------
 
@@ -115,8 +115,8 @@ CREATE TABLE `car_premium` (
 --
 
 INSERT INTO `car_premium` (`Product_id`, `National_id`, `Phone_number`, `Model`, `Bike_condition`, `Rent_Cost`, `Availability`, `Seat`) VALUES
-(40001, 3456789, '01799618574', 'Hyundai Tucson', '1510 KM and good CONDITION', '3000 per day', 'Yes', 4),
-(40002, 1234567, '01748541038', 'Toyota 86 GT', '1200 KM and good CONDITION', '3500 per day', 'Yes', 4);
+(40001, 3456789, '017********', 'Hyundai Tucson', '1510 KM and good CONDITION', '3000 per day', 'Yes', 4),
+(40002, 1234567, '017********', 'Toyota 86 GT', '1200 KM and good CONDITION', '3500 per day', 'Yes', 4);
 
 -- --------------------------------------------------------
 
@@ -139,7 +139,7 @@ CREATE TABLE `checkout` (
 --
 
 INSERT INTO `checkout` (`National_id`, `Product_id`, `Email`, `Phone_number`, `Adress`, `Postal_code`, `Username`) VALUES
-(5666666, 40001, 'abid.hasan@northsouth.edu', '01799618573', 'Sherpur', 2100, 'abidhasan01');
+(5666666, 40001, 'abid.hasan@northsouth.edu', '017********', 'Sherpur', 2100, 'abidhasan01');
 
 -- --------------------------------------------------------
 
@@ -159,7 +159,6 @@ CREATE TABLE `log_in` (
 
 INSERT INTO `log_in` (`user_name`, `pasward`, `nationa_id`) VALUES
 ('abidhasan01', 'aaaaa', 5666666),
-('lanisasiddika', 'iiiii', 4555555),
 ('PranabRoy', 'ppppp', 2333333),
 ('ShaharierSathil', 'sssss', 3456789),
 ('TasbiuslOlive', 'ttttt', 1222222);
@@ -184,7 +183,6 @@ INSERT INTO `owner_info` (`Owner_id`, `National_id`, `Username`) VALUES
 (1, 2345678, 'PranabRoy'),
 (2, 3456789, 'ShaharierSathil'),
 (3, 1234567, 'TasbiuslOlive'),
-(4, 4555555, 'lanisasiddika');
 
 -- --------------------------------------------------------
 
@@ -210,17 +208,17 @@ CREATE TABLE `sign_up` (
 --
 
 INSERT INTO `sign_up` (`First_name`, `last_name`, `User_name`, `National_id`, `Phone_Number`, `Email`, `Adreess`, `Sex`, `Age`, `Pasward`) VALUES
-('Bulbul', 'Hasan', 'Bulbulhasan', 1011111, '01987418916', 'Bulbulhasan55@gmail.com', 'Sherpur', 'Male', 22, 'bbbbb'),
-('Tanzila', 'Rahman', 'Tanzilarahman', 1112121, '01987418917', 'TanzilaRahman55@gmail.com', 'Sherpur', 'Male', 19, 'ttttt'),
-('Tasbiul', 'Olive', 'TasbiuslOlive', 1234567, '01748541038', 'tasbiul.hasan@northsouth.edu', 'Faridpur', 'male', 22, 'ttttt'),
-('Pranab', 'Roy', 'PranabRoy', 2345678, '01765968783', 'pranab.roy@northsouth.edu', 'Bramanbaria', 'male', 22, 'ppppp'),
-('Iqbal', 'Hossain', 'iqbalHoosain', 3444444, '01835047468', 'iqbal.hossain@northsouth.edu', 'Nowakhali', 'Male', 22, 'lllll'),
-('Shaharier', 'Sathil', 'ShaharierSathil', 3456789, '01799618574', 'Shaharier.hoque@northsouth.edu', 'Sherpur', 'male', 22, 'sssss'),
-('Lanisa', 'Siddika', 'lanisasiddika', 4555555, '01758953525', 'laniisaa.siddika@northsouth.edu', 'Comilla', 'Female', 22, 'iiiii'),
-('Abid', 'Hasan', 'abidhasan01', 5666666, '01799618573', 'abid.hasan@northsouth.edu', 'Sherpur', 'Male', 22, 'aaaaa'),
-('Rudnan', 'Ahmeed', 'Rudnanahmmed01', 6777777, '01799526405', 'rudnantanzim@gmail.com', 'Sherpur', 'Male', 17, '2580'),
-('Adiba', 'Abonee', 'AdibaAbonee', 7888888, '01799526405', 'adbibadtasnim69@gmail.com', 'Sherpur', 'Female', 21, 'Addd'),
-('Bishal', 'Azmain', 'BishalAzmain', 8999999, '01625689143', 'bishalazmail84@gmail.com', 'Kisorgong', 'Male', 22, 'bbbb');
+('Bulbul', 'Hasan', 'Bulbulhasan', 1011111, '019********', 'Bulbulhasan55@gmail.com', 'Sherpur', 'Male', 22, 'bbbbb'),
+('Tanzila', 'Rahman', 'Tanzilarahman', 1112121, '019********', 'TanzilaRahman55@gmail.com', 'Sherpur', 'Male', 19, 'ttttt'),
+('Tasbiul', 'Olive', 'TasbiuslOlive', 1234567, '017*******', 'tasbiul.hasan@northsouth.edu', 'Faridpur', 'male', 22, 'ttttt'),
+('Pranab', 'Roy', 'PranabRoy', 2345678, '017********', 'pranab.roy@northsouth.edu', 'Bramanbaria', 'male', 22, 'ppppp'),
+('Iqbal', 'Hossain', 'iqbalHoosain', 3444444, '018********', 'iqbal.hossain@northsouth.edu', 'Nowakhali', 'Male', 22, 'lllll'),
+('Shaharier', 'Sathil', 'ShaharierSathil', 3456789, '017********', 'Shaharier.hoque@northsouth.edu', 'Sherpur', 'male', 22, 'sssss'),
+('Lanisa', 'Siddika', 'lanisasiddika', 4555555, '017********', 'laniisaa.siddika@northsouth.edu', 'Comilla', 'Female', 22, 'iiiii'),
+('Abid', 'Hasan', 'abidhasan01', 5666666, '017********', 'abid.hasan@northsouth.edu', 'Sherpur', 'Male', 22, 'aaaaa'),
+('Rudnan', 'Ahmeed', 'Rudnanahmmed01', 6777777, '017********', 'rudnantanzim@gmail.com', 'Sherpur', 'Male', 17, '2580'),
+('Adiba', 'Abonee', 'AdibaAbonee', 7888888, '017********', 'adbibadtasnim69@gmail.com', 'Sherpur', 'Female', 21, 'Addd'),
+('Bishal', 'Azmain', 'BishalAzmain', 8999999, '016********', 'bishalazmail84@gmail.com', 'Kisorgong', 'Male', 22, 'bbbb');
 
 --
 -- Indexes for dumped tables
